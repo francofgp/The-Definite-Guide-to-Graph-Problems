34383bc0c1565dac137be4745dc602cbb3808068
Giuliano Pértile <giulianopertile@gmail.com>
2022-09-23 14:15:16 -0300

    Fix function name BFS for DFS

--- How do you traverse a graph/DFS Code implementation, Recursion.py
+++ How do you traverse a graph/DFS Code implementation, Recursion.py
@@ -1,11 +1,12 @@
-def breadth_first_search(graph,starting_node):
+def depth_first_search_first_search(graph, starting_node):
     # Create a recursive function that takes in the graph and the starting node.
     print(starting_node)
-    for neighbor in graph[starting_node]: 
+    for neighbor in graph[starting_node]:
         # Create your base cases. In this case my base is when the for loop contains zero elements.
         # When that is the case I do not call the breadth_first_search function
         # Traverse all the neighboring nodes and call the recursive function with the index of the neighboring node.
-        breadth_first_search(graph, neighbor)
+        depth_first_search_first_search(graph, neighbor)
+
 
 graph = {
     1: [2, 3],
@@ -15,5 +16,5 @@ graph = {
     5: [],
     6: [],
 }
-breadth_first_search(graph, 1)
-# 1 3 6 5 2 4 
+depth_first_search_first_search(graph, 1)
+# 1 2 4 3 5 6
--- How do you traverse a graph/Depth-First-Search (DFS).py
+++ How do you traverse a graph/Depth-First-Search (DFS).py
@@ -1,10 +1,13 @@
-def breadth_first_search(graph,starting_node):
-    queue = [starting_node] # Declare a stack and insert the starting node.
-    while len(queue) > 0: # While the stack is not empty you remove the last node of the stack.
-        current_node = queue.pop() 
+def depth_first_search(graph, starting_node):
+    queue = [starting_node]  # Declare a stack and insert the starting node.
+    # While the stack is not empty you remove the last node of the stack.
+    while len(queue) > 0:
+        current_node = queue.pop()
         print(current_node)
         for neighbor in graph[current_node]:
-            queue.append(neighbor) # Insert all the neighbors of the node into the queue.
+            # Insert all the neighbors of the node into the queue.
+            queue.append(neighbor)
+
 
 graph = {
     1: [2, 3],
@@ -14,5 +17,5 @@ graph = {
     5: [],
     6: [],
 }
-breadth_first_search(graph, 1)
+depth_first_search(graph, 1)
 # 1 3 6 5 2 4  # It does not matter if you start with the right neighbor or with the left one
